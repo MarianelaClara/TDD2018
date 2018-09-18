d250acabf55900d1eb6f1c178384486f5df77dc7
Secundario <you@example.com>
2018-09-18 17:35:11 -0300

    Creo funcion vacio

--- src/Mazo.php
+++ src/Mazo.php
@@ -3,8 +3,22 @@
 namespace TDD;
 
 class Mazo {
+protected $cantcartas=0;
 
   public function mezclar() {
     return TRUE;
   }
+
+  public function vacio() {
+    if($cantcartas==0)
+        return TRUE;
+     return FALSE;
+  }
+   
+   
+
+
+
+
+
 }
