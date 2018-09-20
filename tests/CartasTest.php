f0cebcbbe277e1a4c45418e54494068e7d4617c0
Julio Bobe <juliounicornio891@gmail.com>
2018-09-20 18:42:13 -0300

    Agrego test obtnerPalo

--- tests/CartasTest.php
+++ tests/CartasTest.php
@@ -10,4 +10,9 @@ class CartasTest extends TestCase {
 		$cartas = new Cartas;
 		$this->assertTrue(isset($cartas));
 	}
+
+	public function testPalo() {
+		$cartas = new Cartas("Basto", "3");
+		$this->assertEquals($cartas->obtenerPalo(),"Basto");
+	}
 }
