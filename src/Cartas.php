58fbd97a8249422cd5d34ceadc77532cd2976078
MarianelaClara <marianelaclara@gmail.com>
2018-09-20 19:11:14 -0300

    funcion obtener nro

--- src/Cartas.php
+++ src/Cartas.php
@@ -5,15 +5,21 @@ namespace TDD;
 class Cartas {
 
     protected $palo;
+    protected $numero;
 
     public function __construct($palon, $numero){
-    
+        
+        $this->numero= $numero;
         $this->palo= $palon;
 
     }
 
     public function obtenerPalo(){
         return $this->palo;
+    }
+
+    public function obtenerNumero(){
+        return $this->numero;
     }
 }
 
