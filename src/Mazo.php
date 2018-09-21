4931e65350a2eb9554daf8b6fb70374e68932964
Julio Bobe <juliounicornio891@gmail.com>
2018-09-21 16:10:17 -0300

    adapto mazo y agrego funcion sacar

--- src/Mazo.php
+++ src/Mazo.php
@@ -3,26 +3,33 @@
 namespace TDD;
 
 class Mazo {
-  protected $cantcartas=0;
+  protected $cantcartas=-1;
   protected $cajita=[];
   public function mezclar() {
     return TRUE;
   }
 
   public function vacio() {
-    if($this->cantcartas == 0)
-        return TRUE;
-     return FALSE;
+    if($this->cantcartas == -1)
+      return TRUE;
+    return FALSE;
   }
 
   public function agregarCarta($carta) {
-    $this->cajita[$this->cantcartas]= $carta;
     $this->cantcartas ++;
+    $this->cajita[$this->cantcartas]= $carta;
     return TRUE;
   }
 
   public function obtenerCant() {
-    return $this->cantcartas;
+    return ($this->cantcartas +1);
+  }
+
+  public function sacar(){
+    if($this->cantcartas== -1)
+      return FALSE;
+    $this->cantcartas --;
+    return $this->cajita[($this->cantcartas +1)];
   }
 
 }
