6ee41bfba625b0c6523ad04eb2e35978407c69d9
Julio Bobe <juliounicornio891@gmail.com>
2018-10-27 11:05:57 -0300

    implemento mejoras de constructor cartas

--- src/Cartas.php
+++ src/Cartas.php
@@ -7,11 +7,17 @@ class Cartas {
     protected $palo;
     protected $numero;
 
-    public function __construct($palon, $numero){
+    public function __construct($palo, $numero){
         
         $this->numero= $numero;
-        $this->palo= $palon;
-
+        if($palo%4 ==0)
+            $this->palo= "Oro";
+        if($palo%4 ==1)
+            $this->palo= "Basto";
+        if($palo%4 ==2)
+            $this->palo= "Copa";
+        if($palo%4 ==3)
+            $this->palo= "Espada";
     }
 
     public function obtenerPalo(){
