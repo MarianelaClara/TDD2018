d0ccd7ee337b059500e5a67edb89daf2bc0507b3
Julio Bobe <juliounicornio891@gmail.com>
2018-10-27 10:59:50 -0300

    Mejoro construct Cartas

--- tests/CartasTest.php
+++ tests/CartasTest.php
@@ -4,20 +4,30 @@ namespace TDD;
 
 use PHPUnit\Framework\TestCase;
 
-class CartasTest extends TestCase {
+class CartasEspañolasTest extends TestCase {
 
 	public function testExiste(){
-		$cartas = new Cartas("Basto", 3);
+		$cartas = new Cartas(3, 3);
 		$this->assertTrue(isset($cartas));
 	}
 
 	public function testPalo() {
-		$cartas = new Cartas("Basto", 3);
+		$cartas = new Cartas(0, 3);
+		$this->assertEquals($cartas->obtenerPalo(),"Oro");
+		$cartas = new Cartas(1, 3);
+		$this->assertEquals($cartas->obtenerPalo(),"Basto");
+		$cartas = new Cartas(2, 3);
+		$this->assertEquals($cartas->obtenerPalo(),"Copa");
+		$cartas = new Cartas(3, 3);
+		$this->assertEquals($cartas->obtenerPalo(),"Espada");
+		$cartas = new Cartas(4, 3);
+		$this->assertEquals($cartas->obtenerPalo(),"Oro");
+		$cartas = new Cartas(5, 3);
 		$this->assertEquals($cartas->obtenerPalo(),"Basto");
 	}
 
 	public function testNumero() {
-		$cartas = new Cartas("Basto", 3);
+		$cartas = new Cartas(0, 3);
 		$this->assertEquals($cartas->obtenerNumero(), 3);
 	}
 }
